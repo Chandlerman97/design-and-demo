e6e36181596c4c7255723167a09a819670ce3ff0
Chandlerman97 <35213639+Chandlerman97@users.noreply.github.com>
2018-03-11 20:41:04 -0700

    Update slide.js

--- artifacts/slide.js
+++ artifacts/slide.js
@@ -1,8 +1,8 @@
-var myImages =["design-and-demo/artifacts/slides/WP_20140717_21_12_58_Pro.jpg", 
-               "design-and-demo/artifacts/slides/WP_20140812_13_30_26_Pro.jpg",
-               "design-and-demo/artifacts/slides/IMG_3634[835] (2).jpg",
-               "design-and-demo/artifacts/slides/WP_20140507_18_14_09_Pro20140507185635.jpg",
-               "design-and-demo/artifacts/slides/WP_20140507_18_15_45_Pro20140507185603.jpg"];
+var myImages =["slides/WP_20140717_21_12_58_Pro.jpg", 
+               "slides/WP_20140812_13_30_26_Pro.jpg",
+               "slides/IMG_3634[835] (2).jpg",
+               "slides/WP_20140507_18_14_09_Pro20140507185635.jpg",
+               "slides/WP_20140507_18_15_45_Pro20140507185603.jpg"];
 
 var captionImages =["Sunset Downtown","What We Want","Pride","Our City","Living Green"];
 
